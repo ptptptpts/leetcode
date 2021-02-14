d1c9dfa92cf1c9d3a35ce9895978d9af628c21fd
ptptptpts <ptptptpts@gmail.com>
2021-02-15 00:15:16 +0900

    first commit

---
+++ 727/source.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int len = 0;
+
+        for (int i=1; i<nums.length; i++) {
+            
+        }
+
+        return len;
+    }
+}
