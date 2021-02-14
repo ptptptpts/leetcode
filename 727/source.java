bf3d69e9bb90ec0c3327cdc8cb4ca6f62b914b5d
ptptptpts <ptptptpts@gmail.com>
2021-02-15 00:16:08 +0900

    Solve 727

--- 727/source.java
+++ 727/source.java
@@ -1,11 +1,14 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int len = 0;
+        int cursor = 1;
 
         for (int i=1; i<nums.length; i++) {
-            
+            if (nums[i-1] != nums[i]) {
+                nums[cursor] = nums[i];
+                cursor++;
+            }
         }
 
-        return len;
+        return cursor;
     }
 }
