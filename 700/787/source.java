072ea31ab6813bfb816b62f105196875c4c28876
Taesoon Park <ptptptpts@gmail.com>
2022-07-31 22:48:45 +0900

    Solve 787 again

--- 700/787/source.java
+++ 700/787/source.java
@@ -1,38 +1,33 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int[][] bestPrices = new int[n][n];
+        int[]bestPrices = new int[n];
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                bestPrices[i][j] = -1;
-            }
+            bestPrices[i] = -1;
         }
 
         for (int[] flight : flights) {
-            bestPrices[flight[0]][flight[1]] = flight[2];
+            if (flight[0] == src) {
+                bestPrices[flight[1]] = flight[2];
+            }
         }
 
         for (int stops = 0; stops < k; stops++) {
-            int[][] newBestPrices = new int[n][n];
-            for (int i = 0; i < n; i++) {
-                System.arraycopy(bestPrices[i], 0, newBestPrices[i], 0, n);
-            }
+            int[] newBestPrices = new int[n];
+            System.arraycopy(bestPrices, 0, newBestPrices, 0, n);
 
             for (int[] flight : flights) {
                 int flightSrc = flight[0];
                 int flightDst = flight[1];
                 int flightCost = flight[2];
 
-                for (int rootSrc = 0; rootSrc < n; rootSrc++) {
-                    int prevCostToSrc = bestPrices[rootSrc][flightSrc];
-                    if (prevCostToSrc != -1) {
-                        int newCostToDst = prevCostToSrc + flightCost;
-                        int currentCostToDst = newBestPrices[rootSrc][flightDst];
-                        if (currentCostToDst == -1) {
-                            newBestPrices[rootSrc][flightDst] = newCostToDst;
-                        } else {
-                            newBestPrices[rootSrc][flightDst] = Math.min(currentCostToDst,
-                                    newCostToDst);
-                        }
+                int prevCostToSrc = bestPrices[flightSrc];
+                if (prevCostToSrc != -1) {
+                    int newCostToDst = prevCostToSrc + flightCost;
+                    int currentCostToDst = newBestPrices[flightDst];
+                    if (currentCostToDst == -1) {
+                        newBestPrices[flightDst] = newCostToDst;
+                    } else {
+                        newBestPrices[flightDst] = Math.min(currentCostToDst, newCostToDst);
                     }
                 }
             }
@@ -40,6 +35,6 @@ class Solution {
             bestPrices = newBestPrices;
         }
 
-        return bestPrices[src][dst];
+        return bestPrices[dst];
     }
 }
