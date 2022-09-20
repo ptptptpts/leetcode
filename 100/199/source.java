dacfe09afb965dcaf74f8f40e28b88fa31ffc0cf
Taesoon Park <ptptptpts@gmail.com>
2022-09-20 23:04:20 +0900

    Solve 199

---
+++ 100/199/source.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+
+        if (root != null) {
+            LinkedList<TreeNode> currentLevel = new LinkedList<>();
+
+            currentLevel.push(root);
+            while (!currentLevel.isEmpty()) {
+                result.add(currentLevel.getLast().val);
+
+                LinkedList<TreeNode> nextLevel = new LinkedList<>();
+                for (TreeNode node : currentLevel) {
+                    if (node.left != null) {
+                        nextLevel.add(node.left);
+                    }
+                    if (node.right != null) {
+                        nextLevel.add(node.right);
+                    }
+                }
+
+                currentLevel = nextLevel;
+            }
+        }
+
+        return result;
+    }
+
+//    public static class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//        TreeNode() {}
+//        TreeNode(int val) { this.val = val; }
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+}
---
+++ 100/199/source2.java
@@ -0,0 +1,51 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> result = new ArrayList<>();
+
+    public List<Integer> rightSideView(TreeNode root) {
+        visit(root, 0);
+        return result;
+    }
+
+    private void visit(TreeNode node, int level) {
+        if (node != null) {
+            if (result.size() == level) {
+                result.add(node.val);
+            }
+            visit(node.right, level + 1);
+            visit(node.left, level + 1);
+        }
+    }
+
+//    public static class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//        TreeNode() {}
+//        TreeNode(int val) { this.val = val; }
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+}
