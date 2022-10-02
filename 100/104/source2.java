18fc91417717d5bb39a26989aabc2b0dbd270e8b
Taesoon Park <ptptptpts@gmail.com>
2022-10-02 22:25:02 +0900

    Solve 104 again

---
+++ 100/104/source2.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxDepth(TreeNode root) {
+        return findDepth(root);
+    }
+
+    private int findDepth(TreeNode node) {
+        int depth = 0;
+
+        if (node != null) {
+            int leftDepth = findDepth(node.left);
+            int rightDepth = findDepth(node.right);
+            depth = Math.max(leftDepth, rightDepth) + 1;
+        }
+
+        return depth;
+    }
+
+//    public static class TreeNode {
+//        int val;
+//        TreeNode left;
+//        TreeNode right;
+//        TreeNode() {}
+//        TreeNode(int val) { this.val = val; }
+//        TreeNode(int val, TreeNode left, TreeNode right) {
+//            this.val = val;
+//            this.left = left;
+//            this.right = right;
+//        }
+//    }
+}
