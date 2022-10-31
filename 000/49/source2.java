e1c183dc9770e58ed7007748b3c0d96ae4939a46
Taesoon Park <ptptptpts@gmail.com>
2022-10-31 22:15:55 +0900

    Solve 49 again

--- 000/49/source2.java
+++ 000/49/source2.java
@@ -1,3 +1,21 @@
-public class source2 {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> anagramHashMap = new HashMap<>();
+
+        for (String str : strs) {
+            char[] charString = str.toCharArray();
+            Arrays.sort(charString);
+            String keyString = new String(charString);
+
+            List<String> anagrams = anagramHashMap.computeIfAbsent(keyString, k -> new ArrayList<>());
+            anagrams.add(str);
+        }
+
+        return new ArrayList<>(anagramHashMap.values());
+    }
 }
